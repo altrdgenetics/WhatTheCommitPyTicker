b5577773713c491fd6ae412de6386411bae53f05
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-07-30 23:01:36 -0400

    initial setup for the LCD display

---
+++ Ticker.py
@@ -0,0 +1,40 @@
+import random
+import urllib
+import threading
+import Adafruit_CharLCD as LCD
+
+# URL for the commit messages
+link = "http://whatthecommit.com/index.txt"
+
+# Initialize the LCD using the pins
+lcd = LCD.Adafruit_CharLCDPlate()
+lcdLineLimit = 16
+
+
+# Print out the Message To LCD
+def display():
+    threading.Timer(10.0, display).start()
+
+    # Get information from URL
+    f = urllib.urlopen(link)
+    message = f.read()
+
+    if len(message) > lcdLineLimit:
+        message = message[:lcdLineLimit] + "/n" + message[lcdLineLimit:]
+
+    # Setup the LCD for Display
+    red = random.randint(0, 1)
+    green = random.randint(0, 1)
+    blue = random.randint(0, 1)
+
+    if red == 0 and green == 0 and blue == 0:
+        red = 1
+        green = 1
+        blue = 1
+
+    lcd.set_Color(red, green, blue)
+    lcd.clear()
+    lcd.message(message)
+
+
+display()
